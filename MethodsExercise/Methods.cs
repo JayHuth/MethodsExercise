493282ac31b1345b5af5b1b56b402e00d1eed6fd
Justin Huth <Jayhuth96@gmail.com>
2023-02-01 14:18:32 -0600

    finished excersise 2

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Methods
+    {
+        public static int Multiply(int num1, int num2)
+        {
+            int sum = num1 * num2;
+            return sum;
+        }
+        public static int Add(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        public static int Subtract(int num1, int num2) 
+        {
+        int sum = num1 - num2;
+            return sum;
+        }
+
+        public static int Divide(int num1, int num2)
+        { 
+        int sum = num1 / num2;
+            return sum;
+        }
+    }
+}
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("What is your name?"); // output
+            //string name = Console.ReadLine(); // input
+
+            //Console.WriteLine("What is your favorite color?"); // output
+            //string favColor = Console.ReadLine(); // Input
+
+            //Console.WriteLine("What is your favorite animal?"); // input 
+            //string favAnimal = Console.ReadLine(); // output
+
+            //Console.WriteLine("what is your favorite band?"); // input 
+            //string favBand = Console.ReadLine(); // output
+
+            //Console.WriteLine($"Hello {name}, this is your imaginary friend the {favColor} {favAnimal}. Hurry up! Its time for us to see {favBand} live!");
+
+            int answer1 = Methods.Add(2,2);
+            Console.WriteLine(answer1);
+
+            int anwser2 = Methods.Add(5, 6);
+            Console.WriteLine(anwser2);
+
+            int answer3 = Methods.Multiply(2, 4);
+            Console.WriteLine(answer3);
+
+            int answer4 = Methods.Subtract(2, 4);
+            Console.WriteLine(answer4);
+
+            int answer5 = Methods.Divide(6, 3);
+            Console.WriteLine(answer5);
         }
     }
 }
